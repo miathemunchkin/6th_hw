6a89e4451c6b453e5a87fb816b746ae0a6a567b9
miathemunchkin <mia.rokosowski@gmail.com>
2024-06-10 19:38:04 +0300

    fake data inserter created

---
+++ selectors/get_groups.sql
@@ -0,0 +1,2 @@
+SELECT id, student_id, subject_id, value, "date"
+FROM public.grade;
---
+++ test.py
@@ -0,0 +1,7 @@
+def add(number1, number2):
+    return number1+number2
+
+def main():
+    x = 5
+    result = add(x, 7)
+    print(result)
